98c00504376f3f9b9cf1d659200cbf466d4744f9
Sy Khader <sy@sayedkhader.com>
2011-09-13 17:33:24 -0400

    Bleh

--- landing.py
+++ landing.py
@@ -4,6 +4,7 @@
 # Amir Sayed Khader, Qifan Xi
 # askhader@uwaterloo.ca, qxi@uwaterloo.ca
 
+import os
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -19,17 +20,31 @@ class LandingPortal(webapp.RequestHandler):
 
 
 class ServeImage(webapp.RequestHandler):
+
     def get(self):
         img_name = self.request.get('n')
         #self.response.headers['Content-Type'] = 'image/jpeg'
-        self.response.out.write(
+        #self.response.out.write(
+
+
+class ShowAddTemplate(webapp.RequestHandler):
+
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'html/landing.html')
+        self.response.out.write(template.render(path, template_values))
         
+    
+    def post(self):
+
 
 
 def main():
     application = webapp.WSGIApplication([
         (r'/', LandingPortal),
-        (r'/m', ServeImage],
+        (r'/m', ServeImage),
+        (r'/addTemplate', ShowAddTemplate),
+        (r'/procTemplate', ProcAddTemplate)],
     debug=False)
     run_wsgi_app(application)
 
