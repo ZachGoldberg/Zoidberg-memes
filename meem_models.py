64621dc7081a89df4ec036d554826729fc37a38b
Sy Khader <sy@sayedkhader.com>
2011-09-14 16:36:59 -0400

    Resized editor

--- meem_models.py
+++ meem_models.py
@@ -13,3 +13,18 @@ class Template(db.Model):
     name = db.StringProperty()
     img = db.BlobProperty(default=None)
     thumb = db.BlobProperty(default=None)
+
+def get_all_templates():
+    """Return all availabel templates"""
+    ts = {}
+    targets = ['uid', 'name', 'thumb']
+    for t in Template.all():
+        ts[t.uid] = t.name
+    return ts
+
+def get_all_template_ids():
+    """Return a list of template uids"""
+    return [t.uid for t in Template.all()]
+
+def get_template_by_id(uid):
+    return Template.all().filter('uid =', uid).get()
